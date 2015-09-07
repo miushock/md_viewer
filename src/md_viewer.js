85d454e8c944b39a997d58e67c1cb9f29d516b1e
Miushock <miushock@gmail.com>
2015-09-07 05:57:00 +0000

    finished basic functions

--- src/md_viewer.js
+++ src/md_viewer.js
@@ -1,17 +1,46 @@
 define(['domReady!', 'jquery', 'freelog_api', 'markdown'], function(doc, $, freelog_api, markdown){
   var md_viewer = {};
 
-  md_viewer.default_md_content = "Hello.\n\n* markdown placeholder.\n* there's no markdown file for the node to display if you see this.";
-  md_viewer.current_md = md_viewer.default_md_content;
+  md_viewer.default_md_content = "Hello.\n\n *markdown placeholder.\n * there's no markdown file for the node to display if you see this.";
+  md_viewer.current_md = 0; //track the local sequence number of the md to be displayed
   md_viewer.html_content = '';
+  md_viewer.resource_index = [];
 
 
   md_viewer.update_content = function () {
-    this.html_content = markdown.parse(this.current_md);
-    $('#doc-display').html(this.html_content);
+    freelog_api.getResource(md_viewer.resource_index[md_viewer.current_md].resource_id, function(data){
+      md_viewer.html_content = markdown.toHTML(data);
+      $('#doc-display').html(md_viewer.html_content);
+    });
   }
 
-  md_viewer.update_content();
+  $('#md_viewer .button.prev').click(function(){
+    if (md_viewer.current_md > 0) {
+      md_viewer.current_md --;
+      md_viewer.update_content();
+    }
+  });
+
+  $('#md_viewer .button.next').click(function(){
+    if (md_viewer.current_md < md_viewer.resource_index.length-1) {
+      md_viewer.current_md ++;
+      md_viewer.update_content();
+    }
+  });
+
+  md_viewer.init = function () {
+    freelog_api.getResources('markdown', function(resources){
+      md_viewer.resource_index = resources;
+      if (resources.length > 0) {
+        md_viewer.update_content();
+      }
+      else {
+        $('#doc-display').html(md_viewer.html_content);
+      }
+    });
+  }
+
+  md_viewer.init();
 
   return md_viewer;
 });
